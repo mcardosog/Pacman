d93e080879d41da5b668de9907c52280d23c1c74
Marco <cardosogutierrez.marco@gmail.com>
2020-04-21 02:45:53 -0400

    Improved method of recognition. Bilingual enabled. ML trained. Monkey API connected to express server. Need to get the result from the server and perform the action

--- server/routes/pacmanCommunicator.js
+++ server/routes/pacmanCommunicator.js
@@ -1,8 +1,18 @@
 var express = require("express");
 var router = express.Router();
 
-router.get("/", function(req, res, next) {
-res.send("API is working properly");
+const MonkeyLearn = require('monkeylearn')
+const ml = new MonkeyLearn('5f03052dd89ec3b68b2fcb030b7109a6022c2914')
+
+router.get("/:input", function(req, res, next) {
+    const model_id = 'cl_oSrfpf84'
+    let data = [req.params.input];
+    ml.classifiers.classify(model_id, data).then(response => {
+        let x = response.body
+        res.send(x[0].classifications[0].tag_name);
+    }).catch((err)=>{
+       console.log(err);
+    });
 });
 
 module.exports = router;
--- src/AudioControl.js
+++ src/AudioControl.js
@@ -3,19 +3,34 @@ function AudioControl(game) {
     currentGame = game;
 }
 
-var recognition = new webkitSpeechRecognition();//new (window.SpeechRecognition || window.webkitSpeechRecognition || window.mozSpeechRecognition || window.msSpeechRecognition)();
-recognition.lang = 'en-US';
-recognition.interimResults = true;
-recognition.maxAlternatives = 5;
-recognition.continuous = true;
-recognition.start();
+
+var recognitionENG = new webkitSpeechRecognition();//new (window.SpeechRecognition || window.webkitSpeechRecognition || window.mozSpeechRecognition || window.msSpeechRecognition)();
+recognitionENG.lang = 'en-US';
+recognitionENG.interimResults = true;
+recognitionENG.maxAlternatives = 1;
+recognitionENG.continuous = true;
+recognitionENG.start();
+
+var recognitionSPN = new webkitSpeechRecognition();//new (window.SpeechRecognition || window.webkitSpeechRecognition || window.mozSpeechRecognition || window.msSpeechRecognition)();
+recognitionSPN.lang = 'es-US';
+recognitionSPN.interimResults = true;
+recognitionSPN.maxAlternatives = 1;
+recognitionSPN.continuous = true;
+recognitionSPN.start();
+
 
 let gestureStore = {
     'up':KEY_UP,
     'left':KEY_LEFT,
     'right':KEY_RIGHT,
     'down':KEY_DOWN,
-    'enter':KEY_ENTER
+    'enter':KEY_ENTER,
+
+    'arriba':KEY_UP,
+    'izquierda':KEY_LEFT,
+    'derecha':KEY_RIGHT,
+    'abajo':KEY_DOWN,
+    'entrar':KEY_ENTER,
 };
 
 let actionStore = {
@@ -23,42 +38,81 @@ let actionStore = {
     'pill': 2,
     'ghost': 3,
     'cherry': 4,
+
+    'poder': 1,
+    'píldora': 2,
+    'fantasma': 3,
+    'cereza': 4,
 }
 
-recognition.onresult = function(event) {
+
+recognitionSPN.onresult = function(event) {
+    recognitionENG.onresult(event);
+}
+
+var tempBestInterpretation = '';
+var timeStamp = 0;
+
+recognitionENG.onresult = function(event) {
+    console.log(event);
+    if (event.timeStamp - timeStamp >= 2000) {
+        tempBestInterpretation = '';
+        maxConfidence = 0;
+    }
+    var interpretationFinal = false;
+
+    const currentResult = event.results[event.results.length - 1][0];
+    if (currentResult.confidence > 0.75) {
+        const splittedTranscript = currentResult.transcript.split(" ");
+        for (var j = 0; j < splittedTranscript.length; j++) {
+            if (!tempBestInterpretation.includes(splittedTranscript[j])) {
+                tempBestInterpretation = tempBestInterpretation + " " + splittedTranscript[j];
+                timeStamp = event.timeStamp;
+            }
+        }
+    }
+
+    console.log(tempBestInterpretation);
+    callServer(tempBestInterpretation);
+    return;
+
 
     var keyFromAudio = null;
     var actionFromAudio = null;
-    if(!event.results[event.results.length-1].isFinal) { return; }
-    for(i = 0; i < event.results[event.results.length-1].length; i++) {
-        if(keyFromAudio != null || actionFromAudio != null) { break; }
-        let iResult = event.results[event.results.length-1][i];
-        if(keyFromAudio != null) {
+    //if(event.results[event.results.length-1].confidence < 0.7) { return; }
+    for (i = 0; i < event.results[event.results.length - 1].length; i++) {
+        if (keyFromAudio != null || actionFromAudio != null) {
+            break;
+        }
+        let iResult = event.results[event.results.length - 1][i];
+        if (keyFromAudio != null) {
             console.log(keyFromAudio);
             break;
         }
-        for(var key in gestureStore) {
-            if(iResult.transcript.includes(key)) {
+        //callServer(keyFromAudio.transcript);
+        // DELETE
+        for (var key in gestureStore) {
+            if (iResult.transcript.includes(key)) {
                 keyFromAudio = gestureStore[key];
                 break;
             }
             console.log(iResult);
         }
-        for(var key in actionStore) {
-            if(iResult.transcript.includes(key)) {
+        for (var key in actionStore) {
+            if (iResult.transcript.includes(key)) {
                 actionFromAudio = actionStore[key];
                 break;
             }
         }
+        // HERE
     }
 
     if (keyFromAudio != null) {
-        currentGame._keyPressed =  keyFromAudio;
+        currentGame._keyPressed = keyFromAudio;
         currentGame.singleDirection = true;
-    }
-    else if (actionFromAudio != null) {
+    } else if (actionFromAudio != null) {
         var path = null;
-        switch(actionFromAudio) {
+        switch (actionFromAudio) {
             case 1:
                 path = currentGame._scene._pacman.findClosestPowerPellet();
                 console.log('Find a power pill');
@@ -81,3 +135,11 @@ recognition.onresult = function(event) {
     }
     console.log('---    ACTION PROCESSED    ---\n');
 };
+
+//EXAMPLE
+function callServer(transcript) {
+    console.log('Calling The server')
+    fetch("http://localhost:3000/pacmanCommunicator/"+transcript)
+        .then(res => res.text())
+        .then(res => console.log('SERVER RESULT = '+res+'\n'));
+}
