01580c610e9c4d67f39b2cf616dd4a1389423838
Marco <cardosogutierrez.marco@gmail.com>
2020-04-22 13:06:01 -0400

    Finished

--- src/AudioControl.js
+++ src/AudioControl.js
@@ -3,7 +3,6 @@ function AudioControl(game) {
     currentGame = game;
 }
 
-
 var recognitionENG = new webkitSpeechRecognition();//new (window.SpeechRecognition || window.webkitSpeechRecognition || window.mozSpeechRecognition || window.msSpeechRecognition)();
 recognitionENG.lang = 'en-US';
 recognitionENG.interimResults = true;
@@ -45,38 +44,26 @@ let actionStore = {
     'cereza': 4,
 }
 
-
 recognitionSPN.onresult = function(event) {
     recognitionENG.onresult(event);
 }
-
-var tempBestInterpretation = '';
-var timeStamp = 0;
 
 recognitionENG.onresult = function(event) {
 
     var keyFromAudio = null;
     var actionFromAudio = null;
 
-    /*
-    console.log(event);
-    for(i = 0; i < event.results.length; i++) {
-        console.log(`${event.currentTarget.lang} RESULTS [${i}] [${event.results[i].length}]:`);
-        for(j = 0; j < event.results[i].length; j++) {
-            console.log(event.results[i][j].transcript+'\n');
-        }
-    }
-
-     */
-
-
-    //if(event.results[event.results.length-1][0].confidence < 0.5) { return; }
     for(i = 0; i < event.results[event.results.length-1].length; i++) {
         if(keyFromAudio != null || actionFromAudio != null) { break; }
         let iResult = event.results[event.results.length-1][i];
         //iResult.toLowerCase();
         iResult.transcript = iResult.transcript.toLowerCase();
 
+        if(iResult.transcript.includes('ok')) {
+            console.log('XXXXXXXXXXXX');
+            recognitionENG = new webkitSpeechRecognition();
+            recognitionENG.lang = 'es-US';
+        }
 
         for(var key in gestureStore) {
             if(iResult.transcript.includes(key)) {
@@ -127,73 +114,6 @@ recognitionENG.onresult = function(event) {
 
     //console.log('---    ACTION PROCESSED    ---\n');
 };
-
-/*
-recognitionENG.onresult = function(event) {
-    if (event.timeStamp - timeStamp >= 500) {
-        tempBestInterpretation = '';
-        maxConfidence = 0;
-    }
-
-    const currentResult = event.results[event.results.length - 1][0];
-    if (currentResult.confidence > 0.75) {
-        const splittedTranscript = currentResult.transcript.split(" ");
-        for (var j = 0; j < splittedTranscript.length; j++) {
-            if (!tempBestInterpretation.includes(splittedTranscript[j])) {
-                tempBestInterpretation = tempBestInterpretation + " " + splittedTranscript[j];
-                timeStamp = event.timeStamp;
-            }
-        }
-    }
-    else { return; }
-
-    var keyFromAudio = null;
-    var actionFromAudio = null;
-
-        for (var key in gestureStore) {
-            if (tempBestInterpretation.includes(key)) {
-                keyFromAudio = gestureStore[key];
-                break;
-            }
-        }
-        for (var key in actionStore) {
-            if (tempBestInterpretation.includes(key)) {
-                actionFromAudio = actionStore[key];
-                break;
-            }
-        }
-
-    if (keyFromAudio != null) {
-        currentGame._keyPressed = keyFromAudio;
-        currentGame.singleDirection = true;
-    } else if (actionFromAudio != null) {
-        var path = null;
-        switch (actionFromAudio) {
-            case 1:
-                path = currentGame._scene._pacman.findClosestPowerPellet();
-                console.log('Find a power pill');
-                break;
-            case 2:
-                path = currentGame._scene._pacman.findClosestPellet();
-                console.log('Find a pill');
-                break;
-            case 3:
-                path = currentGame._scene._pacman.findClosestVulnerableGhost();
-                console.log('Find a ghost');
-                break;
-            case 4:
-                path = currentGame._scene._pacman.findCherry();
-                console.log('Find the cherry');
-                break;
-        }
-        currentGame.singleDirection = false;
-        currentGame.pathToTaget = path;
-    }
-
-    callServer();
-};
-*/
-//EXAMPLE
 
 function callServer(tempBestInterpretation) {
     console.log('CALLING SERVER')
